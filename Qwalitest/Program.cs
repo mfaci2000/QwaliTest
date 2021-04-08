8baee8c286ed6d5aded7473b9be66fb4fb62d367
mfaci2000 <mfaci2000@yahoo.com>
2021-04-08 09:50:31 -0500

    Initial project

---
+++ Qwalitest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Qwalitest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
